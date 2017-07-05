2c9299f5014d6076835fcf5b2979847afc069077
Simon  Date <simondate@yahoo.co.uk>
2017-07-05 13:48:41 +0100

    Created a runNumber attribute that will append itself onto links.

--- js/adapt-sideView.js
+++ js/adapt-sideView.js
@@ -2,74 +2,85 @@ define(function(require) {
 
   var Adapt = require('coreJS/adapt');
 
-    var IntroView = Backbone.View.extend({
-      initialize: function() {
-        var template = Handlebars.templates.sideView;
-        $('body').addClass('moodle-close');
-        $('html').find('body').append(template());
-        Adapt.trigger("sideView:loaded");
-      },
-    });
+  var IntroView = Backbone.View.extend({
+
+    initialize: function() {
+      var template = Handlebars.templates.sideView;
+      $('body').addClass('moodle-close');
+      $('html').find('body').append(template());
+      console.log(this);
+      var model = this.model;
+
+      Adapt.trigger("sideView:loaded");
+
+      Adapt.once("pageView:ready menuView:ready", function() {
+        if (model._runNumber != "") {
+          console.log('trigger');
+          Adapt.trigger("sideView:appendRun", model._runNumber);
+        }
+      });
+
+      Adapt.on("pageView:ready", function() {
+        Adapt.trigger("sideView:pageReady");
+      });
+
+      Adapt.on("menuView:ready", function() {
+        Adapt.trigger("sideView:menuReady");
+      });
+
+      Adapt.on('sideView:loadIframe', function(iframe, type, url) {
+        document.getElementById(iframe + '-iframe').contentWindow.location.replace(url);
+        $('.sideview-iframe-holder').addClass('loading-iframe');
+        $('.' + iframe + '-iframe').on('load', function() {
+          var adaptCSS = location.protocol + '//' + location.host + location.pathname;
+          adaptCSS = adaptCSS.substring(0, adaptCSS.lastIndexOf('/'));
+          adaptCSS += "/assets/adapt-" + type + ".css";
+          $('.' + iframe + '-iframe').contents().find("head").append($("<link/>", {
+            rel: "stylesheet",
+            href: adaptCSS,
+            type: "text/css"
+          }));
+          document.getElementById(iframe + '-iframe').contentWindow.window.onbeforeunload = null; // prevents error message when leaving moodle page when you haven't submitted.
+          Adapt.trigger('sideView:removeLoading');
+        });
+      });
+
+      Adapt.on('sideView:open', function() {
+        $('body').addClass('moodle-open').removeClass('moodle-close');
+        $('.sideview').removeClass('close').addClass('open');
+      });
+
+      Adapt.on('router:page router:menu', function() {
+        Adapt.trigger('sideView:close');
+      });
+
+
+      Adapt.on('sideView:close', function() {
+        $('.sideview').removeClass('open').addClass('close');
+        $('body').removeClass('moodle-open').addClass('moodle-close');
+        $('.moodle-launch-button.open').removeClass('open');
+      });
+
+      Adapt.on('sideView:removeLoading', function(iframe) {
+        setTimeout(function() {
+          $('.sideview-iframe-holder').removeClass('loading-iframe');
+        }, 400);
+      });
+
+      $(".sideview-controls-close-button").on("click", function() {
+        Adapt.trigger('sideview:close');
+      });
+
+      $(document).on('click', '.sideview-controls-close-button', function() {
+        Adapt.trigger("sideView:close");
+      });
+    }
+  });
 
   Adapt.once('app:dataReady', function() {
-    if (Adapt.course.attributes._sideView._isEnabled)
-      new IntroView({});
-  });
-
-  Adapt.on("pageView:ready", function() {
-    if (Adapt.course.attributes._sideView._isEnabled)
-      Adapt.trigger("sideView:pageReady");
-  });
-
-  Adapt.on("menuView:ready", function() {
-    if (Adapt.course.attributes._sideView._isEnabled)
-      Adapt.trigger("sideView:menuReady");
-  });
-
-  Adapt.on('sideView:loadIframe', function(iframe, type, url) {
-    document.getElementById(iframe + '-iframe').contentWindow.location.replace(url);
-    $('.sideview-iframe-holder').addClass('loading-iframe');
-    $('.' + iframe + '-iframe').on('load', function() {
-      var adaptCSS = location.protocol + '//' + location.host + location.pathname;
-      adaptCSS = adaptCSS.substring(0, adaptCSS.lastIndexOf('/'));
-      adaptCSS += "/assets/adapt-" + type + ".css";
-      $('.' + iframe + '-iframe').contents().find("head").append($("<link/>", {
-        rel: "stylesheet",
-        href: adaptCSS,
-        type: "text/css"
-      }));
-      document.getElementById(iframe + '-iframe').contentWindow.window.onbeforeunload = null; // prevents error message when leaving moodle page when you haven't submitted.
-      Adapt.trigger('sideView:removeLoading');
-    });
-  });
-
-  Adapt.on('sideView:open', function() {
-    $('body').addClass('moodle-open').removeClass('moodle-close');
-    $('.sideview').removeClass('close').addClass('open');
-  });
-
-  Adapt.on('router:page router:menu', function() {
-    Adapt.trigger('sideView:close');
-  });
-
-
-  Adapt.on('sideView:close', function() {
-    $('.sideview').removeClass('open').addClass('close');
-    $('body').removeClass('moodle-open').addClass('moodle-close');
-    $('.moodle-launch-button.open').removeClass('open');
-  });
-
-  Adapt.on('sideView:removeLoading', function(iframe) {
-    setTimeout(function() {
-      $('.sideview-iframe-holder').removeClass('loading-iframe');
-    }, 400);
-  });
-
-  $(".sideview-controls-close-button").on("click", function() {
-    Adapt.trigger('sideview:close');
-  });
-
-  $(document).on('click', '.sideview-controls-close-button', function() {
-    Adapt.trigger("sideView:close");
+    if (Adapt.course.get('_sideView')._isEnabled)
+      new IntroView({
+        model: Adapt.course.get('_sideView')
+      });
   });
 });
